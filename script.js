35c2510313ec5d459281ccd49521e1684fcbc25f
Ariel Scarpinelli <ascarpinelli@spredfast.com>
2023-01-09 14:47:38 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,94 @@
+var days = ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"];
+var months = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'];
+
+var clockElement = document.getElementById("clock");
+var amPmElment = document.getElementById("amPm");
+var dateElement = document.getElementById("date");
+var currentTemperatureElement = document.getElementById("currentTemperature");
+var forecastTemperatureElement = document.getElementById("forecastTemperature");
+
+var currentLat = -34.52;
+var currentLon = -58.49;
+
+var APP_ID = "setyourown";
+
+function showTime() {
+
+    setTimeout(showTime, 1000);
+
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+
+    var amPm = "AM";
+    
+    if (h >= 7 && h <= 21) {
+        document.body.classList.remove("night");
+    } else {
+        document.body.classList.add("night");
+    }
+
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        amPm = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    
+    clockElement.innerText = h + ":" + m;
+    amPmElment.innerText = amPm;
+    dateElement.innerText = days[date.getDay()] + " " + date.getDate() + " " + months[date.getMonth()]
+
+}
+
+function get(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+
+    xhr.onload = function () {
+      if (this.status === 200) {
+        callback(JSON.parse(this.responseText));
+      } else {
+        console.error(this.statusText);
+      }
+    };
+    
+    xhr.send();
+}
+
+function formatTemp(t) {
+    return (Math.round(t * 10) / 10) + "ºC";
+}
+
+function fetchCurrentTemperature() {
+    setTimeout(fetchCurrentTemperature, 1800000);
+    get("https://api.openweathermap.org/data/2.5/weather?lat=" + currentLat + "&lon=" + currentLon + "&appid=" + APP_ID + "&units=metric", function(response) {
+        currentTemperatureElement.innerHTML = formatTemp(response.main.temp);
+    });
+}
+
+function fetchForecastTemperature() {
+    setTimeout(fetchForecastTemperature, 3 * 3600000);
+    get("https://api.openweathermap.org/data/2.5/forecast?lat=" + currentLat + "&lon=" + currentLon + "&appid=" + APP_ID + "&units=metric", function(response) {
+
+        var nextTwelveHours = (Date.now() / 1000) + 24 * 3600
+
+        var temps = response.list
+            .filter(function(item) { return item.dt <= nextTwelveHours })
+            .map(function(item) { return item.main.temp });
+
+        var min = Math.min.apply(null, temps);
+        var max = Math.max.apply(null, temps);
+
+        forecastTemperatureElement.innerHTML = formatTemp(min) + " - " + formatTemp(max);
+    });
+}
+
+showTime();
+fetchCurrentTemperature();
+fetchForecastTemperature();
